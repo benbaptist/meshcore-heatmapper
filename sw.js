334df7d09758f615b151f01a69592778635b4b05
Ben Baptist <sawham6@mgail.com>
2026-08-10 13:26:20 -0400

    bumped sw to v2.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meshcore-heatmapper-v2.2';
+const CACHE = 'meshcore-heatmapper-v2.3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
