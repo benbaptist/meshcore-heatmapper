1b3bb986260247853ee418cc77450e264565519d
Ben Baptist <sawham6@mgail.com>
2026-08-10 19:27:42 -0400

    chore(sw): bump cache to v2.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meshcore-heatmapper-v2.3.1';
+const CACHE = 'meshcore-heatmapper-v2.4';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
